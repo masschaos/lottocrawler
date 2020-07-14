40aaf765e53305af5c16a5475c21e427612e3b7b
Muninn <hyacinthus@gmail.com>
2020-07-14 23:44:59 +0800

    update im

--- crawler/au/latest/powerBallCrawler.js
+++ crawler/au/latest/powerBallCrawler.js
@@ -3,11 +3,11 @@ const moment = require('moment')
 const money_format = require('../../../util/format').money_format
 
 class powerBallCrawler extends crawler {
-    constructor() {
-        super('au-powerball')
-    }
+  constructor () {
+    super('au-powerball')
+  }
 
-    /* 
+  /*
     [
         {
         "drawTime":"20110426000000",
@@ -24,26 +24,26 @@ class powerBallCrawler extends crawler {
         }
     ]
     */
-    parse(data) {
-        let item = {
-            "drawTime": moment(data.DrawDate).format('YYYYMMDDHHmmss'),
-            "detail": data.Dividends.map(a => {
-                let result = {
-                    name: a.Division,
-                    prize: a.BlocDividend > 0 ? money_format(a.BlocDividend, 2, ".", ",", "$") : "",
-                    count: a.BlocNumberOfWinners
-                }
-                return result
-            }),
-            "jackpot": [],
-            "other": [],
-            "issue": data.DrawNumber,
-            "numbers": [data.PrimaryNumbers.join(','), data.SecondaryNumbers.join(',')].join('|'),
-            "name": "Powerball",
-            "lotteryID": "au-powerball"
+  parse (data) {
+    const item = {
+      drawTime: moment(data.DrawDate).format('YYYYMMDDHHmmss'),
+      detail: data.Dividends.map(a => {
+        const result = {
+          name: a.Division,
+          prize: a.BlocDividend > 0 ? money_format(a.BlocDividend, 2, '.', ',', '$') : '',
+          count: a.BlocNumberOfWinners
         }
-        return item
+        return result
+      }),
+      jackpot: [],
+      other: [],
+      issue: data.DrawNumber,
+      numbers: [data.PrimaryNumbers.join(','), data.SecondaryNumbers.join(',')].join('|'),
+      name: 'Powerball',
+      lotteryID: 'au-powerball'
     }
+    return item
+  }
 }
 
 module.exports = powerBallCrawler
