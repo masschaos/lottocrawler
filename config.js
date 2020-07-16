bb5ae40dd13eb559f63e8fc12a684b22247e61a3
Muninn <hyacinthus@gmail.com>
2020-07-16 11:42:25 +0800

    cron config

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 const config = {
   baseURL: process.env.BASE_URL || 'https://seaapi.lottowawa.com/staging',
   token: process.env.TOKEN,
+  cron: process.env.CRON || '0 5 * * * *',
   env: process.env.DEPLOYMENT_ENV,
   pptrEnv: process.env.PPTR_ENV
 }
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const CronJob = require('cron').CronJob
 const run = require('./index')
+const { cron } = require('./config')
 
 // 每个小时的 5分开始一轮检查
-var job = new CronJob('0 5 * * * *', run)
+var job = new CronJob(cron, run)
 job.start()
