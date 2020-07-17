d9368e75ea7c869f0387e6d164b631ab57188795
Muninn <hyacinthus@gmail.com>
2020-07-17 23:03:34 +0800

    better log

--- util/log.js
+++ util/log.js
@@ -1,6 +1,12 @@
 const pino = require('pino')
 const { level } = require('../config')
 
-const log = pino({ level })
+const log = pino({
+  level,
+  // 在启用日志收集系统之前，格式化日志以便查看
+  prettyPrint: {
+    translateTime: true
+  }
+})
 
 module.exports = log
