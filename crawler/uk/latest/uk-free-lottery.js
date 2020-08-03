902417a756b2420dbb10afb62245cad704400d3d
guowenjia <601152819@qq.com>
2020-08-03 21:18:32 +0800

    fix uk free lottery

--- crawler/uk/latest/uk-free-lottery.js
+++ crawler/uk/latest/uk-free-lottery.js
@@ -26,8 +26,9 @@ const Craw1 = async (page, dataObj) => {
   // get time
   const dateStr = await page.$eval(dateSelector, el => el.innerText)
   console.log(dateStr)
-  const [day, month, year] = dateStr.split(' ')
-  const drawTime = `${year}${MonthOrDayProcess(MONTH[month.slice(0, -1)])}${MonthOrDayProcess(day)}140000`
+  let [day, month, year] = dateStr.split(' ')
+  month = month.trim().replace(',', '')
+  const drawTime = `${year}${MonthOrDayProcess(MONTH[month])}${MonthOrDayProcess(day)}140000`
 
   // get number and jackpot
   const numberStr = await page.$eval(numberSelector, el => el.innerText)
@@ -53,7 +54,7 @@ const Craw2 = async (page, dataObj) => {
   // get time
   const dateStr = await page.$eval(dateSelector, el => el.innerText)
   let [day, month, year] = dateStr.split(' ')
-  month = month.slice(0, -1)
+  month = month.trim().replace(',', '')
   console.log(month)
   const drawTime = `${year}${MonthOrDayProcess(MONTH[month])}${MonthOrDayProcess(day)}193000`
 
