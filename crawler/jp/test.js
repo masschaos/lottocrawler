98adba99d3386fd9d16090b8606c6d0bfd7da874
mapleincode <llkj13579@126.com>
2020-08-06 03:24:49 +0800

    test: add test example

---
+++ crawler/jp/test.js
@@ -0,0 +1,16 @@
+/**
+ * @Author: maple
+ * @Date: 2020-08-06 03:17:41
+ * @LastEditors: maple
+ * @LastEditTime: 2020-08-06 03:22:37
+ */
+const crawlers = require('./index')
+
+async function done () {
+  for (const [name, datas] of crawlers) {
+    console.log(name)
+    console.log(JSON.stringify(await datas[0].crawl()))
+  }
+}
+
+done()
