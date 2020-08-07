9b0903a9f773f20e7e23494bd65eb31297e4c463
mapleincode <llkj13579@126.com>
2020-08-07 13:33:52 +0800

    fix: fix crawler name jp-loto6 redundant space

--- crawler/jp/index.js
+++ crawler/jp/index.js
@@ -2,7 +2,7 @@
  * @Author: maple
  * @Date: 2020-08-05 20:58:46
  * @LastEditors: maple
- * @LastEditTime: 2020-08-06 02:31:30
+ * @LastEditTime: 2020-08-07 13:32:24
  */
 
 const Bingo5 = require('./crawlers/jp-bingo5')
@@ -16,7 +16,7 @@ const Miniloto = require('./crawlers/jp-miniloto')
 const crawlers = new Map()
 crawlers.set('jp-bingo5', [Bingo5])
 crawlers.set('jp-kisekaeqoochan', [Kisekaeqoochan])
-crawlers.set('jp-loto6 ', [Loto6])
+crawlers.set('jp-loto6', [Loto6])
 crawlers.set('jp-loto7', [Loto7])
 crawlers.set('jp-numbers3', [Numbers3])
 crawlers.set('jp-numbers4', [Numbers4])
