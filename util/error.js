bb00066ee82a2fdeccbcdc83487731c96ecadaee
Muninn <hyacinthus@gmail.com>
2020-08-21 20:54:25 +0800

    remove unused files

--- util/error.js
+++ util/error.js
@@ -1,5 +1,5 @@
 class SiteClosedError extends Error {
-  constructor(lotteryID) {
+  constructor (lotteryID) {
     const message = `网站处于停业时间。彩种: ${lotteryID}`
     super(message)
     this.name = 'SiteClosedError'
@@ -7,7 +7,7 @@ class SiteClosedError extends Error {
 }
 
 class DrawingError extends Error {
-  constructor(lotteryID) {
+  constructor (lotteryID) {
     const message = `正在开奖中，无法获取结果。彩种: ${lotteryID}`
     super(message)
     this.name = 'DrawingError'
