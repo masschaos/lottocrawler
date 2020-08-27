e53189a97c515b21da30c1547f3fa29563324830
Muninn <hyacinthus@gmail.com>
2020-08-27 10:50:49 +0800

    npm run dev add country arg support

--- dev.js
+++ dev.js
@@ -1,3 +1,15 @@
 const run = require('./scheduler')
 
-run()
+switch (process.argv.length) {
+  case 2:
+    run()
+    break
+  case 3:
+    run(process.argv[2])
+    break
+  case 4:
+    run(process.argv[2], process.argv[3])
+    break
+  default:
+    throw new Error('invalid arguments')
+}
