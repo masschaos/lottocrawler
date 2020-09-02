10faab8479d9872ef43af57681ec482177e1eb58
mapleincode <llkj13579@126.com>
2020-09-03 00:52:09 +0800

    fix: keno add drawing error

--- crawler/fr/test.js
+++ crawler/fr/test.js
@@ -3,14 +3,13 @@
  * @Author: maple
  * @Date: 2020-08-15 21:11:35
  * @LastEditors: maple
- * @LastEditTime: 2020-09-02 23:18:38
+ * @LastEditTime: 2020-09-03 00:49:22
  */
 const crawlers = require('./index')
 const log = require('../../util/log')
 
 async function done () {
   for (const [name, datas] of crawlers) {
-    if (name !== 'fr-loto') continue
     log.debug(`%${name}%`)
     log.debug(JSON.stringify(await datas[0].crawl()))
   }
