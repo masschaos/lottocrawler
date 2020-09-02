2297c0f77b2f94dc8228fa9a886c1f4100345a6c
mapleincode <llkj13579@126.com>
2020-09-03 00:41:59 +0800

    feat: fr-loto step crawl

--- crawler/fr/test.js
+++ crawler/fr/test.js
@@ -3,13 +3,14 @@
  * @Author: maple
  * @Date: 2020-08-15 21:11:35
  * @LastEditors: maple
- * @LastEditTime: 2020-08-15 21:11:58
+ * @LastEditTime: 2020-09-02 23:18:38
  */
 const crawlers = require('./index')
 const log = require('../../util/log')
 
 async function done () {
   for (const [name, datas] of crawlers) {
+    if (name !== 'fr-loto') continue
     log.debug(`%${name}%`)
     log.debug(JSON.stringify(await datas[0].crawl()))
   }
