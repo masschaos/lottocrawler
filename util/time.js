7228bb86806bec71227d7707e2c5575a0f45f89a
guowenjia <601152819@qq.com>
2020-09-10 18:57:49 +0800

    fix bug

--- util/time.js
+++ util/time.js
@@ -50,19 +50,10 @@ function isExtraReady (drawTime, delay, tz) {
   return parseDrawTime(drawTime, tz).add(delay, 's').isBefore(moment().tz(tz))
 }
 
-function wait (second) {
-  const promise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve()
-    }, second)
-  })
-  return promise
-}
 
 module.exports = {
   sleep,
   wait,
   hasNewDraw,
   isExtraReady,
-  wait
 }
